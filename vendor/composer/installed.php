fd710ad2096ef9a0eb03acbfccd66025e76168fb
Anderson Casimiro <casimiro.arruda@gmail.com>
2026-04-15 11:59:11 -0300

    feat: initialize project structure with Composer dependencies and PDS skeleton

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'andr/chm-tide-reader',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'andr/chm-tide-reader' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pds/skeleton' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '95e476e5d629eadacbd721c5a9553e537514a231',
+            'type' => 'standard',
+            'install_path' => __DIR__ . '/../pds/skeleton',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
